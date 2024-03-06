35b7b48405dbd8b75dc9ceccad769584005319d5
Frankral <rafranckyamb04@gmail.com>
2024-03-06 19:41:00 +0300

    adding exectime

---
+++ src/pages/coding/minimalCode.js
@@ -0,0 +1,7 @@
+export function getinitialC(){
+    return "#include <stdio.h>\n#include <stdlib.h>\n\nint main()\n{\n\tprintf(\"Hello World!\");\n\treturn 0;\n}"
+}
+
+export function getinitialJs(){
+    return "console.log(\"Hello World!\");"
+}
